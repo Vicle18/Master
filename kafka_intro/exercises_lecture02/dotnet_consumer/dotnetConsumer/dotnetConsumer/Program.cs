ae147defeafa8ef7b3dca8430f4f386d69d594c2
sieben8nein <bende1308@gmail.com>
2023-02-08 11:52:33 +0100

    added kafka intro

---
+++ kafka_intro/exercises_lecture02/dotnet_consumer/dotnetConsumer/dotnetConsumer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Confluent.Kafka;
+
+namespace KafkaConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = "kafka1:9092",
+                GroupId = "example-consumer-group",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using (var consumer = new ConsumerBuilder<Null, string>(config).Build())
+            {
+                consumer.Subscribe("example_topic");
+
+                Console.WriteLine("Kafka Consumer is running...");
+
+                while (true)
+                {
+                    try
+                    {
+                        var message = consumer.Consume();
+
+                        Console.WriteLine($"Message received: '{message.Value}' from partition {message.Partition} with offset {message.Offset}");
+                    }
+                    catch (ConsumeException e)
+                    {
+                        Console.WriteLine($"Consumption failed: {e.Error.Reason}");
+                    }
+                }
+            }
+        }
+    }
+}
