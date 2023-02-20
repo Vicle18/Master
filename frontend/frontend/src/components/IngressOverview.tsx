5cbfd7cd8ba279352e626aa2aff9c3da0adb2859
sieben8nein <bende1308@gmail.com>
2023-02-20 12:33:13 +0100

    added search bar

--- frontend/frontend/src/components/IngressOverview.tsx
+++ frontend/frontend/src/components/IngressOverview.tsx
@@ -1,155 +1,93 @@
-import * as React from 'react';
-import SvgIcon, { SvgIconProps } from '@mui/material/SvgIcon';
-import { alpha, styled } from '@mui/material/styles';
-import TreeView from '@mui/lab/TreeView';
-import TreeItem, { TreeItemProps, treeItemClasses } from '@mui/lab/TreeItem';
-import Collapse from '@mui/material/Collapse';
-import { useSpring, animated } from '@react-spring/web';
-import { TransitionProps } from '@mui/material/transitions';
-import { gql, useQuery } from '@apollo/client';
+import * as React from "react";
+import { gql, useQuery } from "@apollo/client";
+import SearchBar from "./IngressOverviewSearch";
+import CustomizedTreeView from './IngressOverviewTree';
+import { useState } from "react";
 
-function MinusSquare(props: SvgIconProps) {
-  return (
-    <SvgIcon fontSize="inherit" style={{ width: 14, height: 14 }} {...props}>
-      {/* tslint:disable-next-line: max-line-length */}
-      <path d="M22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0zM17.873 11.023h-11.826q-.375 0-.669.281t-.294.682v0q0 .401.294 .682t.669.281h11.826q.375 0 .669-.281t.294-.682v0q0-.401-.294-.682t-.669-.281z" />
-    </SvgIcon>
-  );
-}
-
-function PlusSquare(props: SvgIconProps) {
-  return (
-    <SvgIcon fontSize="inherit" style={{ width: 14, height: 14 }} {...props}>
-      {/* tslint:disable-next-line: max-line-length */}
-      <path d="M22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0zM17.873 12.977h-4.923v4.896q0 .401-.281.682t-.682.281v0q-.375 0-.669-.281t-.294-.682v-4.896h-4.923q-.401 0-.682-.294t-.281-.669v0q0-.401.281-.682t.682-.281h4.923v-4.896q0-.401.294-.682t.669-.281v0q.401 0 .682.281t.281.682v4.896h4.923q.401 0 .682.281t.281.682v0q0 .375-.281.669t-.682.294z" />
-    </SvgIcon>
-  );
-}
-
-function CloseSquare(props: SvgIconProps) {
-  return (
-    <SvgIcon
-      className="close"
-      fontSize="inherit"
-      style={{ width: 14, height: 14 }}
-      {...props}
-    >
-      {/* tslint:disable-next-line: max-line-length */}
-      <path d="M17.485 17.512q-.281.281-.682.281t-.696-.268l-4.12-4.147-4.12 4.147q-.294.268-.696.268t-.682-.281-.281-.682.294-.669l4.12-4.147-4.12-4.147q-.294-.268-.294-.669t.281-.682.682-.281.696 .268l4.12 4.147 4.12-4.147q.294-.268.696-.268t.682.281 .281.669-.294.682l-4.12 4.147 4.12 4.147q.294.268 .294.669t-.281.682zM22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0z" />
-    </SvgIcon>
-  );
-}
-
-function TransitionComponent(props: TransitionProps) {
-  const style = useSpring({
-    from: {
-      opacity: 0,
-      transform: 'translate3d(20px,0,0)',
-    },
-    to: {
-      opacity: props.in ? 1 : 0,
-      transform: `translate3d(${props.in ? 0 : 20}px,0,0)`,
-    },
-  });
-
-  return (
-    <animated.div style={style}>
-      <Collapse {...props} />
-    </animated.div>
-  );
-}
-
-const StyledTreeItem = styled((props: TreeItemProps) => (
-  <TreeItem {...props} TransitionComponent={TransitionComponent} />
-))(({ theme }) => ({
-  [`& .${treeItemClasses.iconContainer}`]: {
-    '& .close': {
-      opacity: 0.3,
-    },
-  },
-  [`& .${treeItemClasses.group}`]: {
-    marginLeft: 15,
-    paddingLeft: 18,
-    borderLeft: `1px dashed ${alpha(theme.palette.text.primary, 0.4)}`,
-  },
-}));
+type Props = {
+  onItemClick: (data: any) => void;
+};
 
 const GET_LOCATIONS = gql`
   query GetAreas {
     companies {
+      name
+      plants {
         name
-        plants {
+        areas {
           name
-          areas {
+          lines {
             name
-            lines {
+            cells {
               name
-              cells {
+              machines {
                 name
-                machines {
-                  name
-                }
               }
             }
           }
         }
       }
     }
+  }
 `;
 
 interface TreeNode {
-    name: string;
-    [key: string]: any;
+  name: string;
+  [key: string]: any;
 }
 
-const findChildrenKey = (node: TreeNode): string | undefined => {
-    const keys = Object.keys(node);
-    for (let i = 0; i < keys.length; i++) {
-        const key = keys[i];
-        if (Array.isArray(node[key]) && node[key].length > 0) {            
-            return key;
-        }
+function extractNames(nodes: TreeNode[]) {
+  const names: string[] = [];
+  const traverse = (node: TreeNode) => {
+    names.push(node.name);
+    const childrenKey = findChildrenKey(node);
+    if (childrenKey && Array.isArray(node[childrenKey])) {
+      node[childrenKey]?.forEach((child: TreeNode) => {
+        traverse(child);
+      });
     }
-    return undefined;
-};
-
-
-const renderTree = (nodes: TreeNode[], onItemClick: (data: any) => void) => {
-    return nodes.map((node) => {
-        
-        const childrenKey = findChildrenKey(node);
-        return (
-        <StyledTreeItem key={node.name} nodeId={node.name} label={node.name} onClick={() => onItemClick(node.name)}>
-            {childrenKey && renderTree(node[childrenKey], onItemClick)}
-        </StyledTreeItem>
-        );
+  };
+  if (Array.isArray(nodes)) {
+    nodes.forEach((node) => {
+      traverse(node);
     });
+  } else {
+    console.log("Error: nodes argument is not an array.");
+  }
+  return names;
+}
+const findChildrenKey = (node: TreeNode): string | undefined => {
+  const keys = Object.keys(node);
+  for (let i = 0; i < keys.length; i++) {
+    const key = keys[i];
+    if (Array.isArray(node[key]) && node[key].length > 0) {
+      return key;
+    }
+  }
+  return undefined;
 };
 
-interface TreeViewProps {
-    onItemClick: (data: any) => void;
-}
 
-const CustomizedTreeView: React.FC<TreeViewProps> = ({onItemClick}) => {
-    const { loading, error, data } = useQuery(GET_LOCATIONS);
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error : {error.message}</p>;
-    // console.log(data);
-  
-  
-    return (
-      <TreeView
-        aria-label="customized"
-        defaultExpanded={['1']}
-        defaultCollapseIcon={<MinusSquare />}
-        defaultExpandIcon={<PlusSquare />}
-        defaultEndIcon={<CloseSquare />}
-        sx={{ height: 264, flexGrow: 1, maxWidth: 400, overflowY: 'auto' }}
-      >
-          {renderTree(data.companies, onItemClick)}
-      </TreeView>
-    );
-}
+const IngressOverview: React.FC<Props> = ({ onItemClick }) => {
+  const [searchString, setSearchString] = useState('');
 
 
-export default CustomizedTreeView;
+  const { loading, error, data } = useQuery(GET_LOCATIONS);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+
+
+  function onSearchResultSelection(searchString: string) {
+    setSearchString(searchString);
+  }
+
+
+  return (
+    <>
+      <SearchBar suggestions={extractNames(data)} onSearch={onSearchResultSelection}/>
+      <CustomizedTreeView onItemClick={onItemClick} searchString={searchString} data={data}/>
+    </>
+  );
+};
+
+export default IngressOverview;
--- frontend/frontend/src/views/Overview/OverviewPage.tsx
+++ frontend/frontend/src/views/Overview/OverviewPage.tsx
@@ -1,11 +1,12 @@
 import React, { FC, useState } from 'react';
-import CustomizedTreeView from '../../components/IngressOverview';
+import CustomizedTreeView from '../../components/IngressOverviewTree';
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid2 from '@mui/material/Unstable_Grid2';
 import DetailedView from '../../components/IngressDetailed';
 import Chart from '../../components/DataChart';
+import IngressOverview from '../../components/IngressOverview';
 interface OverviewProps {
   title: string;
 }
@@ -27,20 +28,33 @@ const Overview: FC<OverviewProps> = ({ title }) => {
 
   return (
     <>
-      <Box sx={{ flexGrow: 6 }}>
-      <Grid2 container spacing={2}>
-        <Grid2 xs={3}>
-          <CustomizedTreeView onItemClick={handleItemClick} />
+      <Box sx={{ flexGrow: 6, height: '90vh' }}>
+      <Grid2 container spacing={2} sx={{ height: '100%' }}>
+        <Grid2 xs={2} sx={{ 
+          marginTop: '30px', 
+          marginLeft: '20px', 
+          marginRight: '20px',
+          borderRadius: '10px', 
+          backgroundColor: 'whitesmoke'  }}>
+            <IngressOverview onItemClick={handleItemClick}/>
         </Grid2>
-        <Grid2 xs={5.5}>
+        <Grid2 xs={5.2} sx={{ 
+          marginTop: '30px', 
+          marginRight: '20px',
+          borderRadius: '10px', 
+
+          backgroundColor: 'whitesmoke'  }}>
           <DetailedView containingEntityId={selectedItemData}/>
         </Grid2>
-        <Grid2 xs={3.5}>
-            <Chart url="http://localhost:5001/data" refreshInterval={1000} />
+        <Grid2 xs sx={{ 
+          marginTop: '30px', 
+          marginRight: '50px', 
+          borderRadius: '10px', 
+          backgroundColor: 'whitesmoke'   }}>
+            <Chart url="http://localhost:5001/data" refreshInterval={10000} />
         </Grid2>
       </Grid2>
     </Box>
-      
     </>
   );
 };
