4e699a11826631c0344689bf61c6350a4ec749f0
Victor <vicle18@student.sdu.dk>
2023-04-04 10:31:47 +0200

    Ingress updates timestamps of error and update

--- WatchDog/WatchDog/Models/ObservableProperty.cs
+++ WatchDog/WatchDog/Models/ObservableProperty.cs
@@ -5,4 +5,8 @@ public class ObservableProperty
     public string id { get; set; }
 
     public string? status { get; set; }
+
+    public string? lastUpdatedAt { get; set; }
+
+    public string? errorStateAt { get; set; }
 }
--- WatchDog/WatchDog/Repositories/IngressRepository.cs
+++ WatchDog/WatchDog/Repositories/IngressRepository.cs
@@ -13,6 +13,7 @@ public class IngressRepository : IIngressRepository
     private readonly IConfiguration _config;
     private readonly ILogger<IngressRepository> _logger;
     private GraphQLHttpClient graphQLClient;
+    private bool _hasErrorOccured = false;
 
     public IngressRepository(IConfiguration config, ILogger<IngressRepository> logger)
     {
@@ -47,16 +48,15 @@ public class IngressRepository : IIngressRepository
 
         Log.Debug(JsonSerializer.Serialize(response.Data));
         return response.Data.ObservableProperties.Select(op => op.id).ToList();
-        ;
     }
 
-    public async Task<bool> updateObservableStatus(string id, bool active)
+    public async Task<bool> updateObservableStatus(string id, bool active, DateTime lastUpdatedAt)
     {
         Log.Debug("Updating");
         var variables = new
         {
             where = new { id = $"{id}" },
-            update = new { status = $"{active}" }
+            update = new { status = $"{active}", lastUpdatedAt = $"{lastUpdatedAt}" }
         };
 
         // Define the GraphQL mutation request
@@ -67,14 +67,46 @@ public class IngressRepository : IIngressRepository
                             observableProperties {
                                 id
                                 status
+                                lastUpdatedAt
                             }
                         }
                     }",
             Variables = variables
         };
-        
+
+        if (!active && !_hasErrorOccured)
+        {
+            UpdateErrorAt(id, lastUpdatedAt);
+        }
+
         var response = await graphQLClient.SendMutationAsync<ObservableProperty>(mutation);
         Log.Debug(JsonSerializer.Serialize(response));
         return true;
+    }
+
+    private async Task UpdateErrorAt(string id, DateTime lastUpdatedAt)
+    {
+        var variables = new
+        {
+            where = new { id = $"{id}" },
+            update = new { errorStateAt = $"{lastUpdatedAt}" }
+        };
+
+        var mutation = new GraphQLRequest
+        {
+            Query = @"
+              mutation($where: ObservablePropertyWhere, $update: ObservablePropertyUpdateInput) {
+                updateObservableProperties(where: $where, update: $update) {
+                  observableProperties {
+                    id
+                    errorStateAt
+                  }
+                }
+              }",
+            Variables = variables
+        };
+        var response = await graphQLClient.SendMutationAsync<ObservableProperty>(mutation);
+        Log.Debug(JsonSerializer.Serialize(response));
+        _hasErrorOccured = true;
     }
 }
