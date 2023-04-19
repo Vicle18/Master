b73fa672ea9b231f4c3ae1b0705dd63ddb26db2b
vicle18 <vicle18@student.sdu.dk>
2023-04-19 22:15:11 +0200

    node name

--- frontend/frontend/src/components/Ingress/IngressDetailed.tsx
+++ frontend/frontend/src/components/Ingress/IngressDetailed.tsx
@@ -300,7 +300,7 @@ const DetailedView: React.FC<IDetailedViewProps> = ({
       initialValues.port = connectionDetails.PARAMETERS.PORT.toString()
       initialValues.host = connectionDetails.PARAMETERS.HOST.toString()
     } else if (connectionDetails.PROTOCOL == "OPCUA") {
-      initialValues.nodeId = connectionDetails.PARAMETERS.NODEID
+      initialValues.nodeId = connectionDetails.PARAMETERS.NODE_NAME
       // TODO SØRG FOR VED UPDATE I MIDDLE_WARE AT REQUEST ET KILL POD OSV. OG LAVE EN NY MED DE NYE CONNECTIONDETAILS
     }
     initialValues.name = data.name
